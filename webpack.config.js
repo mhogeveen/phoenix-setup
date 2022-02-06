b9e098f077103abde2bb977b552538e2487af31c
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-02-06 14:35:35 +0100

    Clean up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,70 +6,54 @@ import TerserPlugin from "terser-webpack-plugin";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// ----------------------------
-// Configuration: Entries
-// ----------------------------
-const entry = {
-  "phoenix": path.resolve(__dirname, "src/main.js"),
+export default (env) => {
+  // ----------------------------
+  // Configuration: Entries
+  // ----------------------------
+  const entry = {
+    "phoenix": path.resolve(__dirname, "src/main.js"),
+  };
+
+  // ----------------------------
+  // Configuration: Output
+  // ----------------------------
+  const output = {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  };
+
+  // ----------------------------
+  // Configuration: Optimization
+  // ----------------------------
+  const optimization = {
+    minimizer: [
+      new TerserPlugin({
+        minify: TerserPlugin.swcMinify,
+      }),
+    ],
+  };
+
+  // ----------------------------
+  // Configuration: Watch Options
+  // ----------------------------
+  const watchOptions = {
+    aggregateTimeout: 500,
+    ignored: "/node_modules/",
+  };
+
+  // ----------------------------
+  // Configuration: Mode
+  // ----------------------------
+  const mode = env.mode;
+
+  const config = {
+    entry,
+    output,
+    optimization,
+    watchOptions,
+    mode,
+  };
+
+  return config;
 };
-
-// ----------------------------
-// Configuration: Output
-// ----------------------------
-const output = {
-  filename: "[name].js",
-  path: path.resolve(__dirname, "dist"),
-  clean: true,
-};
-
-// ----------------------------
-// Configuration: Modules
-// ----------------------------
-const module = {};
-
-// ----------------------------
-// Configuration: Plugins
-// ----------------------------
-const plugins = [];
-
-// ----------------------------
-// Configuration: Devtool
-// ----------------------------
-const devtool = false;
-
-// ----------------------------
-// Configuration: Optimization
-// ----------------------------
-const optimization = {
-  minimizer: [
-    new TerserPlugin({
-      minify: TerserPlugin.swcMinify,
-    }),
-  ],
-};
-
-// ----------------------------
-// Configuration: Watch Options
-// ----------------------------
-const watchOptions = {
-  aggregateTimeout: 500,
-  ignored: "/node_modules/",
-};
-
-// ----------------------------
-// Configuration: Mode
-// ----------------------------
-const mode = "production";
-
-const config = {
-  entry,
-  output,
-  module,
-  // plugins,
-  // devtool,
-  optimization,
-  watchOptions,
-  mode,
-};
-
-export default config;
